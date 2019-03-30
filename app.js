80b30a080d4e4b2f882385f39c042481ec9f0c74
stkoev <st.koev@outlook.com>
2019-03-30 23:02:54 +0200

    Layer 3 Protection - dotenv - use environment variables

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //jshint esversion:6
+
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +9,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+// console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -21,7 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "This is our little secret.";
+const secret = process.env.SECRET;
 userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
