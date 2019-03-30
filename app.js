d07c734760ef96c6fc03123f32ae058edbc14736
stkoev <st.koev@outlook.com>
2019-03-30 23:35:06 +0200

    Layer 3 Protection - add Hashing - md5

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 //jshint esversion:6
 
-require('dotenv').config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -27,7 +26,6 @@ const userSchema = new mongoose.Schema({
 });
 
 const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -47,7 +45,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (!err) {
@@ -61,7 +59,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({
     email: username
   }, function(err, foundUser) {
